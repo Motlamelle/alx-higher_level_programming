0c3ebd968dafd7870059b7809eb734f75bdc355b
Motlamelle <lebomotlamelle@gmail.com>
2022-06-02 20:36:44 +0200

    import cal module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, cal.sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, cal.mul(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, cal.div(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
