3b797d8be253b0692e8df8b4960f5954a32a4ad2
Motlamelle <lebomotlamelle@gmail.com>
2022-06-05 21:00:39 +0200

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
+        i = 0
+        ln = len(row)-1
         for col in row:
-            print("{:d} ".format(col), end="")
+            if i < ln:
+                print("{:d} ".format(col), end="")
+                i=i+1
+            else:
+                print("{:d}".format(col), end="")
         print("")
