55aa39ac0ed023b0456e7f448135daf4faa2dda1
Motlamelle <lebomotlamelle@gmail.com>
2022-06-05 21:59:14 +0200

    Find max integer of a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    if len(my_list) == 0:
+        return None
+    else:
+        for val in my_list:
+            if val >= max:
+                max = val
+    return max
