17a6ba1a03fd561a89a1865113b357f892ae2b78
Motlamelle <lebomotlamelle@gmail.com>
2022-07-05 19:50:06 +0200

    pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This function reads a text file and prints to STDOUT
+"""
 
 
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints to stdout"""
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
