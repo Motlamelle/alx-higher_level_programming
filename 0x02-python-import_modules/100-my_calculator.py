33992afaa4ecb527263129b2c1b8ef34682f72a4
Motlamelle <lebomotlamelle@gmail.com>
2022-10-05 23:51:34 +0200

    fix checkers

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def calc():
+    from calculator_1 import add, sub, mul, div
     from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    ops = ["+", "-", "*", "/"]
-    from calculator_1 import add, sub, mul, div
-    funcs = [add, sub, mul, div]
-    for i, s in enumerate(ops):
-        if argv[2] == s:
-            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
-            break
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        quit(1)
+        exit(1)
+
+
+if __name__ == '__main__':
+    calc()
